759c2695212b9312507467bf28a1b48ef52eaeae
Shadman Kolahzary <shadman.ko@gmail.com>
2019-10-04 03:54:59 +0330

    logger interceptor

---
+++ src/app/_infrastructure/logger.interceptor.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+import { MessageService } from 'primeng/components/common/messageservice';
+
+@Injectable()
+export class LoggerInterceptor implements HttpInterceptor {
+  constructor(
+    private messageService: MessageService
+  ) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      tap((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          this.messageService.add({
+            severity:'info',
+            summary:'Http Call Success',
+            detail: event.url});
+
+            console.log(['http success response', event])
+          }
+      }),
+      catchError((event: HttpEvent<any>) => {
+        if (event instanceof HttpErrorResponse) {
+          console.log(['http error response', event]);
+          if (event.status === 0) {
+            return throwError('Could not connect to server');
+          }
+          if (event.error === undefined) {
+            console.error(['empty response!', event, event.error]);
+            return throwError('Empty response');
+          }
+          if (event.error === null) {
+            return throwError(event.statusText);
+          }
+        }
+      }));
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+
+import { LoggerInterceptor } from './_infrastructure/logger.interceptor';
+
 import {
   TodosComponent,
   TodoFormComponent } from './panel';
@@ -19,9 +25,13 @@ import {
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastModule
   ],
-  providers: [],
+  providers: [
+    MessageService,
+    { provide: HTTP_INTERCEPTORS, useClass: LoggerInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
